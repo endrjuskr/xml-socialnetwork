cf3a9f336a44465c60afcffb9a5bc98b4dd30fa3
Andrzej Skrodzki <andrzej.skrodzki@gmail.com>
2014-12-06 00:09:50 -0800

    Modify first page.

--- WorkoutDiaryPhoneApp/WorkoutDiaryPhoneApp/DataViewController.swift
+++ WorkoutDiaryPhoneApp/WorkoutDiaryPhoneApp/DataViewController.swift
@@ -8,31 +8,28 @@
 
 import UIKit
 
-class DataViewController: UIViewController {
-
-    @IBOutlet weak var dataLabel: UILabel!
-    var dataObject: AnyObject?
-
-
+class DataViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-    override func viewWillAppear(animated: Bool) {
-        super.viewWillAppear(animated)
-        if let obj: AnyObject = dataObject {
-            self.dataLabel!.text = obj.description
-        } else {
-            self.dataLabel!.text = ""
-        }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section:    Int) -> Int {
+        return 10
     }
-
-
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: "MyTestCell")
+        
+        cell.textLabel?.text = "Row #\(indexPath.row)"
+        cell.detailTextLabel?.text = "Subtitle #\(indexPath.row)"
+        
+        return cell
+    }
 }
 
