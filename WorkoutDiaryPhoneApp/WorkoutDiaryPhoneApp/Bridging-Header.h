a8f8b0045f5ab14813317be63c5dbc94ba06cd2d
Andrzej Skrodzki <andrzej.skrodzki@gmail.com>
2014-12-09 22:31:56 -0800

    Add Facebook login.

---
+++ WorkoutDiaryPhoneApp/WorkoutDiaryPhoneApp/Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  Bridging-Header.h
+//  WorkoutDiaryPhoneApp
+//
+//  Created by Andrzej Skrodzki on 12/9/14.
+//  Copyright (c) 2014 Andrzej Skrodzki. All rights reserved.
+//
+
+#ifndef WorkoutDiaryPhoneApp_Bridging_Header_h
+#define WorkoutDiaryPhoneApp_Bridging_Header_h
+
+#import <FacebookSDK/FacebookSDK.h>
+
+#endif
--- WorkoutDiaryPhoneApp/WorkoutDiaryPhoneApp/MenuViewController.swift
+++ WorkoutDiaryPhoneApp/WorkoutDiaryPhoneApp/MenuViewController.swift
@@ -8,10 +8,35 @@
 
 import UIKit
 
-class MenuViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+class MenuViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, FBLoginViewDelegate {
+    
+    @IBOutlet var fbLoginView : FBLoginView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.fbLoginView.delegate = self
+        self.fbLoginView.readPermissions = ["public_profile", "email", "user_friends"]
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    func loginViewShowingLoggedInUser(loginView : FBLoginView!) {
+        println("User Logged In")
+    }
+    
+    func loginViewFetchedUserInfo(loginView : FBLoginView!, user: FBGraphUser) {
+        println("User: \(user)")
+        println("User ID: \(user.objectID)")
+        println("User Name: \(user.name)")
+        var userEmail = user.objectForKey("email") as String
+        println("User Email: \(userEmail)")
+    }
+    
+    func loginViewShowingLoggedOutUser(loginView : FBLoginView!) {
+        println("User Logged Out")
+    }
+    
+    func loginView(loginView : FBLoginView!, handleError:NSError) {
+        println("Error: \(handleError.localizedDescription)")
     }
     
     override func didReceiveMemoryWarning() {
