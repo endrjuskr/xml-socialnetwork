f6943b0e629d5061e687bc7f6587fc47c46673e4
Andrzej Skrodzki <andrzej.skrodzki@gmail.com>
2014-12-10 23:32:50 -0800

    New view controllers.

---
+++ WorkoutDiaryPhoneApp/WorkoutDiaryPhoneApp/NewExerciseViewController.swift
@@ -0,0 +1,9 @@
+//
+//  NewExerciseViewController.swift
+//  WorkoutDiaryPhoneApp
+//
+//  Created by Andrzej Skrodzki on 12/10/14.
+//  Copyright (c) 2014 Andrzej Skrodzki. All rights reserved.
+//
+
+import Foundation
