56d98ce688fe43babbab2943d5caadddbdffcbd6
Andrzej Skrodzki <andrzej.skrodzki@gmail.com>
2014-12-13 22:57:35 -0800

    Query collection.

--- WorkoutDiaryRestWebService/src/main/java/com/WorkoutDiary/web/service/controllers/AuthController.java
+++ WorkoutDiaryRestWebService/src/main/java/com/WorkoutDiary/web/service/controllers/AuthController.java
@@ -5,6 +5,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.xmldb.api.DatabaseManager;
 import org.xmldb.api.base.Collection;
+import org.xmldb.api.base.Resource;
+import org.xmldb.api.base.ResourceIterator;
+import org.xmldb.api.base.ResourceSet;
 import org.xmldb.api.base.XMLDBException;
 import org.xmldb.api.modules.XPathQueryService;
 
@@ -19,6 +22,14 @@ public class AuthController {
     	Collection col = DatabaseManager.getCollection(Config.DatabaseURI);
         XPathQueryService xpqs = (XPathQueryService)col.getService("XPathQueryService", "1.0");
         xpqs.setProperty("indent", "yes");
+        xpqs.setNamespace("", "http://workout-diary.com");
+        ResourceSet result = xpqs.query("doc('/db/apps/WorkoutDiary/WorkoutDiaryDatabase.xml')/database/users");
+        ResourceIterator i = result.getIterator();
+        Resource res = null;
+        while(i.hasMoreResources()) {
+	        res = i.nextResource();
+	        System.out.println(res.getContent());
+        }
         return true;
     }
 }
