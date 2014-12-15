48788a93c059e41d7c8856fbfab0a1f8042ca5b2
Andrzej Skrodzki <andrzej.skrodzki@gmail.com>
2014-12-14 23:44:55 -0800

    Add navigation.

--- WorkoutDiaryPhoneApp/WorkoutDiaryPhoneApp/LoginViewController.swift
+++ WorkoutDiaryPhoneApp/WorkoutDiaryPhoneApp/LoginViewController.swift
@@ -8,12 +8,42 @@
 
 import UIKit
 
-class LoginViewController: UIViewController {
+class LoginViewController: UIViewController, FBLoginViewDelegate {
+    @IBOutlet var fbLoginView : FBLoginView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.fbLoginView.delegate = self
+        self.fbLoginView.readPermissions = ["public_profile"]
     }
-    
+
+
+    func finishedWithAuth(auth: GTMOAuth2Authentication!, error: NSError!) {
+        println(auth)
+    }
+
+    func loginViewShowingLoggedInUser(loginView : FBLoginView!) {
+        println("User Logged In")
+    }
+
+    @IBAction func loginViewFetchedUserInfo(loginView : FBLoginView!, user: FBGraphUser) {
+        println("User ID: \(user.objectID)")
+        self.performSegueWithIdentifier("testS", sender: nil)
+        let menuViewController = MenuViewController(nibName: "MenuViewController", bundle: nil)
+
+        menuViewController.userID = user.objectID
+
+        self.navigationController?.pushViewController(menuViewController, animated: true)
+    }
+
+    func loginViewShowingLoggedOutUser(loginView : FBLoginView!) {
+        println("User Logged Out")
+    }
+
+    func loginView(loginView : FBLoginView!, handleError:NSError) {
+        println("Error: \(handleError.localizedDescription)")
+    }
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
--- WorkoutDiaryPhoneApp/WorkoutDiaryPhoneApp/MenuViewController.swift
+++ WorkoutDiaryPhoneApp/WorkoutDiaryPhoneApp/MenuViewController.swift
@@ -7,69 +7,21 @@
 //
 
 import UIKit
-import AddressBook
-import MediaPlayer
-import AssetsLibrary
-import CoreLocation
-import CoreMotion
 
-class MenuViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, FBLoginViewDelegate, GPPSignInDelegate {
-    
-    @IBOutlet var fbLoginView : FBLoginView!
-    @IBOutlet var signInGoogleP : GPPSignIn?
+class MenuViewController: UIViewController {
+    var userID: String = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.fbLoginView.delegate = self
-        self.fbLoginView.readPermissions = ["public_profile", "email", "user_friends"]
+        FBRequestConnection.startWithGraphPath("me/picture", completionHandler: {
+            (FBRequestConnection connection, id result, NSError error) -> Void in
+            let a = ""
+        })
+    }
 
-        signInGoogleP = GPPSignIn.sharedInstance()
-        signInGoogleP?.shouldFetchGooglePlusUser = true
-        signInGoogleP?.clientID = "clientID"
-        signInGoogleP?.scopes = [ kGTLAuthScopePlusLogin ];
-        signInGoogleP?.delegate = self;
-    }
-    
-    func finishedWithAuth(auth: GTMOAuth2Authentication!, error: NSError!) {
-        println(auth)
-    }
-    
-    func loginViewShowingLoggedInUser(loginView : FBLoginView!) {
-        println("User Logged In")
-    }
-    
-    func loginViewFetchedUserInfo(loginView : FBLoginView!, user: FBGraphUser) {
-        println("User: \(user)")
-        println("User ID: \(user.objectID)")
-        println("User Name: \(user.name)")
-        var userEmail = user.objectForKey("email") as String
-        println("User Email: \(userEmail)")
-    }
-    
-    func loginViewShowingLoggedOutUser(loginView : FBLoginView!) {
-        println("User Logged Out")
-    }
-    
-    func loginView(loginView : FBLoginView!, handleError:NSError) {
-        println("Error: \(handleError.localizedDescription)")
-    }
-    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-    }
-    
-    func tableView(tableView: UITableView, numberOfRowsInSection section:    Int) -> Int {
-        return 10
-    }
-    
-    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell: UITableViewCell = UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: "MyTestCell")
-        
-        cell.textLabel?.text = "Row #\(indexPath.row)"
-        cell.detailTextLabel?.text = "Subtitle #\(indexPath.row)"
-        
-        return cell
     }
 }
 
